51ba9e836b46c8a8fe54e2e29f9be1b125777a5d
BcDemir <burak.demir@deop.ca>
2023-05-31 23:04:52 -0400

    Jump and sound fixed - can pick up valuable box

--- Assets/Scripts/ValuablesBox.cs
+++ Assets/Scripts/ValuablesBox.cs
@@ -7,7 +7,11 @@ public class ValuablesBox : MonoBehaviour, ICollectible
 {
     public static event Action OnValuablesBoxCollected;
     private Rigidbody rb;
-    public void Collect()
+
+	public bool pickedUp = false,
+				vulnerability = false;
+
+	public void Collect()
     {
         //throw new System.NotImplementedException();
         Debug.Log("Valuables Box Secured!");
@@ -28,6 +32,12 @@ public class ValuablesBox : MonoBehaviour, ICollectible
 		{
 			Debug.LogWarning("Rigidbody component not found on the game object.");
 		}
+	}
+
+	// Invoke by player to remove damage at first pick up
+	private void DamageToBox()
+	{
+		vulnerability = true;
 	}
 
 }
